502e7b1312dcc0db8b16e12766ff09e59871224b
LifeStrange <892536707@qq.com>
2022-01-17 20:57:52 +0800

    update: index.hmtl title delete: minix

--- src/mixins/index.ts
+++ src/mixins/index.ts
@@ -2,9 +2,9 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn' // import locale
 import isLeapYear from 'dayjs/plugin/isLeapYear' // import plugin
 import relativeTime from 'dayjs/plugin/relativeTime' // import plugin
-import CustomParseFormat from 'dayjs/plugin/CustomParseFormat' // import plugin
+// import CustomParseFormat from 'dayjs/plugin/CustomParseFormat' // import plugin
 dayjs.extend(relativeTime) // use plugin
-dayjs.extend(CustomParseFormat) // use plugin
+// dayjs.extend(CustomParseFormat) // use plugin
 dayjs.extend(isLeapYear) // use plugin
 
 dayjs.locale('zh-cn') // use locale
