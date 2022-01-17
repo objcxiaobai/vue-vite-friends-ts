8def9cf2e7d77d9862f9bd8c409362e99039b9d9
LifeStrange <892536707@qq.com>
2022-01-17 23:08:46 +0800

    update: mock

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { setupProdMockServer } from '../mock/mockProdServer';
+
+if (import.meta.env.VITE_APP_COMMAND === 'build') {
+    setupProdMockServer();
+}
 createApp(App).mount('#app')
